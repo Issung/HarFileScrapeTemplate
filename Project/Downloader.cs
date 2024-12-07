fa5f7afa4f5e4cff6fb811f746c2deeef67f25fb
Issung <joel@sonofgoran.com>
2024-12-07 20:54:53 +1100

    Initial commit

---
+++ Project/Downloader.cs
@@ -0,0 +1,42 @@
+﻿namespace Project;
+
+public static class Downloader
+{
+    public static async Task Download(string destinationPath, List<Uri> urls, int maxParallelism)
+    {
+        using var httpClient = new HttpClient();
+
+        var parallelOptions = new ParallelOptions
+        {
+            MaxDegreeOfParallelism = maxParallelism,
+        };
+
+        var success = 0;
+        var failure = 0;
+
+        Directory.CreateDirectory(destinationPath);
+
+        await Parallel.ForEachAsync(urls, parallelOptions, async (url, cancellationToken) =>
+        {
+            try
+            {
+                var path = Path.Combine(destinationPath, Path.GetFileName(url.AbsolutePath));
+
+                var bytes = await httpClient.GetByteArrayAsync(url, cancellationToken);
+                await File.WriteAllBytesAsync(path, bytes, cancellationToken);
+
+                Console.WriteLine($"Saved: {path}");
+                success++;
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Failed to download {url}. Error: {ex.Message}");
+                Console.ResetColor();
+                failure++;
+            }
+        });
+
+        Console.WriteLine($"{success} downloads completed successfully, {failure} failed.");
+    }
+}
---
+++ Project/Program.cs
@@ -0,0 +1,17 @@
+﻿using HarSharp;
+using Project;
+
+const string harPath = "C:\\Users\\example\\Downloads\\example.har";
+const string destinationPath = "C:\\Users\\example\\Downloads\\HarFileScrapes";
+var har = HarConvert.DeserializeFromFile(harPath);
+
+var urls = Scraper.Scrape(har);
+
+Console.WriteLine(string.Join("\n\t", urls));
+Console.Write($"Count: {urls.Count}.");
+
+await Downloader.Download(
+    destinationPath,
+    urls,
+    maxParallelism: 5
+);
